2e4501a592408b21d57f5593b5dadebb5562f8e2
bat <zoe@kittycat.homes>
2024-04-17 17:53:38 +0200

    suspend search batr

--- app.vue
+++ app.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="p-0 h-svh flex flex-col w-full items-stretch gap-6">
-    <SearchBar class="fixed w-full backdrop-blur-xl" />
+    <Suspense>
+      <SearchBar class="fixed w-full backdrop-blur-xl" />
+    </Suspense>
     <div class="overflow-y-scroll w-full flex justify-center">
       <div class="pt-24 prose">
         <NuxtPage />
