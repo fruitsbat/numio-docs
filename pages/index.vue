d31b0d5ed9fd7ec6135c94dcace70abb0f4f3ee6
bat <zoe@kittycat.homes>
2024-04-24 15:55:32 +0200

    flex wrap

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <ProseH1>Welcome to NumIO ensembles!</ProseH1>
-  <div class="flex w-full justify-between">
+  <div class="flex flex-wrap w-full justify-between">
     <NuxtLink class="btn btn-ghost" to="/getting-started">get started</NuxtLink>
     <NuxtLink class="btn btn-ghost" to="/config">configuration</NuxtLink>
     <NuxtLink class="btn btn-ghost" to="/results">evaluating results</NuxtLink>
